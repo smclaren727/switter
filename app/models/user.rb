cb0d40f3577b9055ce92ae8135849e35b807c12c
smclaren727 <smclaren727@gmail.com>
2015-09-06 10:32:21 -0400

    Fixed indentations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
-	before_save { self.email = email.downcase }
-	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 },
-										format: { with: VALID_EMAIL_REGEX },
-										uniqueness: { case_sensitive: false } 
-	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false } 
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -36,4 +36,4 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
- end
+end
